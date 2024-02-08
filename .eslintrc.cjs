162627949f1922e006da29dbdbd96e27da9b691b
Dominic Tölle <dominic.tolle@gmail.com>
2024-02-08 14:26:36 +0100

    Add linting
    - add husky
    
    Ticket: #4

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+/* eslint-env node */
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: { project: ['./tsconfig.json'] },
+  root: true,
+  ignorePatterns: ['src/**/*.spec.ts', 'dist/*']
+};
