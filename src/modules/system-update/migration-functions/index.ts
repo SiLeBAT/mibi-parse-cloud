7f213fe2ac944c438173f9abdfbfc6ea299bc89b
Dominic Tölle <dominic.tolle@gmail.com>
2024-05-30 11:02:22 +0200

    Add NRL Schema creation
    Ticket: #39

--- src/modules/system-update/migration-functions/index.ts
+++ src/modules/system-update/migration-functions/index.ts
@@ -1,4 +1,5 @@
 import { mp35CreateUserInfo } from './mp35-create-user-info';
+import { mp39CreateNRL } from './mp39-create-nrl';
 
 export interface UpdateFunc {
     (): Promise<boolean>;
@@ -7,4 +8,7 @@ export interface UpdateFunc {
  * New functions to extend or alter the db schema should be added here, at the BOTTOM of the array.
  * DO NOT REMOVE OR ALTER THE EXISTING ARRAY ENTRIES.
  */
-export const UPDATE_FUNCTION_ARRAY: UpdateFunc[] = [mp35CreateUserInfo];
+export const UPDATE_FUNCTION_ARRAY: UpdateFunc[] = [
+    mp35CreateUserInfo,
+    mp39CreateNRL
+];
