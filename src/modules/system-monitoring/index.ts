19c5ee5e477d487d865141d45255f8a5cc59b03e
Dominic Tölle <dominic.tolle@gmail.com>
2024-07-01 10:32:35 +0200

    Add code comment to system update and system monitoring modules

--- src/modules/system-monitoring/index.ts
+++ src/modules/system-monitoring/index.ts
@@ -1,3 +1,8 @@
+/**
+ * The system monitoring module includes functions that:
+ * - through an api give details about the health and state
+ * of the running system
+ */
 import { logger } from '../../system/logging';
 import './useCases';
 logger.info('Parse Cloud: System monitoring module loaded.');
