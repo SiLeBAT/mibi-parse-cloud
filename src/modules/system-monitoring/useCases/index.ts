749c3e44bd0216378675fe3a3c2a46e66f03749c
Dominic Tölle <dominic.tolle@gmail.com>
2024-10-04 07:30:55 +0200

    Cache update after NRL deletion
    Ticket: #82

--- src/modules/system-monitoring/useCases/index.ts
+++ src/modules/system-monitoring/useCases/index.ts
@@ -2,10 +2,15 @@ import { logger } from '../../../system/logging';
 import { ObjectKeys } from '../infrastructure/parse-types';
 import { checkSystemConfiguration } from './checkSystemConfiguration';
 import './getSystemInformation';
-import { afterSaveNRLHook, setNRLCache } from './setNRLCache';
+import {
+    afterSaveNRLHook,
+    afterDeleteNRLHook,
+    setNRLCache
+} from './setNRLCache';
 logger.info('Parse Cloud: Checking System Configuration.');
 checkSystemConfiguration.execute();
 logger.info('Parse Cloud: Creating NRL cache.');
 setNRLCache.execute();
 
 Parse.Cloud.afterSave(ObjectKeys.NRL, afterSaveNRLHook);
+Parse.Cloud.afterDelete(ObjectKeys.NRL, afterDeleteNRLHook);
